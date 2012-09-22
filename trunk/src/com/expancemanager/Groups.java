c8ab158d5c75f816f59c50092ffc38a3036df30f
brijesh.masrani@gmail.com <brijesh.masrani@gmail.com@7d377409-a469-d689-ca0f-66ec55f633e5>
2012-09-22 15:53:10 +0000

    Created Project

---
+++ trunk/src/com/expancemanager/Groups.java
@@ -0,0 +1,20 @@
+package com.expancemanager;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Groups extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_groups, menu);
+        return true;
+    }
+}
